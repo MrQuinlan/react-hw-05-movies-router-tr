f18b78175b29b56771b78dae4bdeebca44f60e9b
Ivan Kiiashko <ivan.m.kiiashko@gmail.com>
2024-02-29 20:48:21 -0500

    Update CastList.jsx

--- src/Components/CastList/CastList.jsx
+++ src/Components/CastList/CastList.jsx
@@ -4,27 +4,27 @@ const CastList = () => {
   return (
     <ul>
       <li>
-        <img alt="Actor photo"></img>
+        <img alt="Actor photo0"></img>
         <h3>Name</h3>
         <p>Character: rembo</p>
       </li>
       <li>
-        <img alt="Actor photo"></img>
+        <img alt="Actor photo1"></img>
         <h3>Name</h3>
         <p>Character: rembo</p>
       </li>
       <li>
-        <img alt="Actor photo"></img>
+        <img alt="Actor photo2"></img>
         <h3>Name</h3>
         <p>Character: rembo</p>
       </li>
       <li>
-        <img alt="Actor photo"></img>
+        <img alt="Actor photo3"></img>
         <h3>Name</h3>
         <p>Character: rembo</p>
       </li>
       <li>
-        <img alt="Actor photo"></img>
+        <img alt="Actor photo4"></img>
         <h3>Name</h3>
         <p>Character: rembo</p>
       </li>
